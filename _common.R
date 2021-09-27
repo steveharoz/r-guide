243cc9d564cae63b8a4e2b568d6866ff6b1dbd10
Steve Haroz <steveharoz@users.noreply.github.com>
2021-09-27 14:49:22 +0200

    style and formatting

---
+++ _common.R
@@ -0,0 +1,9 @@
+set.seed(8675309)
+
+knitr::opts_chunk$set(
+  class.output = "code_result",
+  class.message = c("code_result", "message"),
+  class.warning = c("code_result", "warning"),
+  class.error = c("code_result", "error"),
+  comment = "#>"
+)
